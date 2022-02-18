728c1bfb420b1b7afe8ae4fd1c9adc7da6a1a5c6
valentinarau <vraurobayo@frba.utn.edu.ar>
2022-02-18 13:21:03 -0300

    elody-run game

---
+++ Melody-Run/app.js
@@ -0,0 +1,91 @@
+document.addEventListener('DOMContentLoaded',()=>{
+const melody = document.querySelector('.melody');
+const grid = document.querySelector('.grid');
+const alert = document.getElementById('alert');
+
+let jumping = false;
+let gravity = 0.9;
+let gameOver = false;
+
+function control(e) {
+    if(e.keyCode === 32){
+        if (!jumping) {
+            jumping = true;
+            jump();
+          }
+    }
+
+}
+let position = 0;
+document.addEventListener('keyup',control)
+function jump() {
+    count = 0;
+    let timerId = setInterval(function (){
+        //down
+        if (count === 15){
+            clearInterval(timerId);
+            let downTimerId = setInterval(function(){
+                if(count   === 0) {
+                    clearInterval(downTimerId);
+                    jumping = false;
+                }
+                else {
+                    position -= 5;
+                    count --;
+                    position = position*gravity;
+                    melody.style.bottom = position + "px";
+                }
+            },20)
+        }
+        //up
+        position += 25;
+        count ++;
+        position = position*gravity;
+        melody.style.bottom = position + "px"; 
+        },20)
+}
+
+function generateKuromis() {
+    let randomTime = 1000 + Math.random() * 4000;
+    let kuromiPosition = 2000;
+    const kuromi = document.createElement('div');
+    kuromi.classList.add('kuromi');
+    grid.appendChild(kuromi);  // makes an append of the kuromi into the grid
+    kuromi.style.left = kuromiPosition + "px";
+    generateFrutillita();
+    let timerId = setInterval(function() {
+        if (kuromiPosition > 0 && kuromiPosition < 60 && position <= 100) {
+            clearInterval(timerId);
+            gameOver = true;            
+            alert.innerHTML = 'Game Over';
+            //remove all children
+            while (grid.firstChild) {
+                grid.removeChild(grid.lastChild)
+            }
+            }
+        kuromiPosition -= 10;
+        kuromi.style.left = kuromiPosition + "px";
+        },20)
+        if (!gameOver) setTimeout(generateKuromis, randomTime);
+}
+
+function generateFrutillita(){
+    let frutillitaPosition = 1000 + Math.random()*4000;
+    const frutillita = document.createElement('div');
+    frutillita.classList.add('frutillita');
+    grid.appendChild(frutillita);  // makes an append of the frutillita into the grid
+    frutillita.style.left = frutillitaPosition + "px";
+
+    let timerId = setInterval(function() {
+        if (frutillitaPosition > 0 && frutillitaPosition < 20 && position >= 50) {
+            //remove all children
+            grid.removeChild(frutillita);
+            }
+        frutillitaPosition -= 10;
+        frutillita.style.left = frutillitaPosition + "px";
+        },20)
+}
+generateFrutillita();
+generateKuromis()
+
+})
